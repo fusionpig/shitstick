ab0ec199ccb29e51f88d9e51101fad515de9f94d
FusionPig <vince@fusionpig.com>
2021-07-01 15:31:34 -0400

    HEllo
    yup

--- src/pages/index.js
+++ src/pages/index.js
@@ -9,7 +9,7 @@ const IndexPage = () => (
   <Layout>
     <Seo title="Home" />
     <h1>Hi people</h1>
-    <p>Welcome to hell.</p>
+    <p>Welcome to hello.</p>
     <p>Now go build something great.</p>
     <StaticImage
       src="../images/gatsby-astronaut.png"
